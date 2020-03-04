c651efe9a78e1eb2dc5bac0ae6977c8b6dd4113c
Nicholas Krsikapa <krsinich589@gmail.com>
2020-03-04 03:03:09 -0500

    Improved Search Bar component

--- app/src/main/java/com/example/recyclingapp/HomeFragment.java
+++ app/src/main/java/com/example/recyclingapp/HomeFragment.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -15,82 +13,80 @@ import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
-
+    View v;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View v = inflater.inflate(R.layout.fragment_home, container,false);
-
+        v = inflater.inflate(R.layout.fragment_home, container,false);
         Context mCtx = getActivity().getApplicationContext();
 
+//        Toolbar toolbar = v.findViewById(R.id.home_toolbar);
 
 
-        //Temporary data and code that will be removed and replaced with database
-        ItemClass item = new ItemClass("Paper","Recycling");
-        ItemClass item1 = new ItemClass("Food","Compost");
-        ItemClass item2 = new ItemClass("Tin foil","Garbage");
-        ItemClass item3 = new ItemClass("Paint","Hazard");
-        ItemClass item4 = new ItemClass("Cardboard","Recycling");
-        ItemClass item5 = new ItemClass("Food","Compost");
-        ItemClass item6 = new ItemClass("Garbage","Garbage");
-        ItemClass item7 = new ItemClass("Batteries","Hazard");
-        ItemClass item8= new ItemClass("Paper","Recycling");
-        ItemClass item9 = new ItemClass("Food","Compost");
-        ItemClass item0 = new ItemClass("Garbage","Garbage");
-        ItemClass item11 = new ItemClass("Batteries","Hazard");
-        ItemClass item12 = new ItemClass("Paper","Recycling");
-        ItemClass item13 = new ItemClass("Food","Compost");
-        ItemClass item14 = new ItemClass("Garbage","Garbage");
-        ItemClass item15 = new ItemClass("Batteries","Hazard");
-        ItemClass item16 = new ItemClass("Paper","Recycling");
-        ItemClass item17 = new ItemClass("Food","Compost");
-        ItemClass item18 = new ItemClass("Garbage","Garbage");
-        ItemClass item19 = new ItemClass("Batteries","Hazard");
 
-        ArrayList<ItemClass> overallList = new ArrayList<>();
-
-        overallList.add(item1);overallList.add(item11);
-        overallList.add(item2);overallList.add(item12);
-        overallList.add(item3);overallList.add(item14);
-        overallList.add(item4);overallList.add(item15);
-        overallList.add(item5);overallList.add(item16);
-        overallList.add(item6);overallList.add(item17);
-        overallList.add(item7);overallList.add(item18);
-        overallList.add(item8);overallList.add(item19);
-        overallList.add(item9);overallList.add(item13);
-        overallList.add(item0);overallList.add(item);
+//        //Temporary data and code that will be removed and replaced with database
+//        ItemClass item = new ItemClass("Paper","Recycling");
+//        ItemClass item1 = new ItemClass("Food","Compost");
+//        ItemClass item2 = new ItemClass("Tin foil","Garbage");
+//        ItemClass item3 = new ItemClass("Paint","Hazard");
+//        ItemClass item4 = new ItemClass("Cardboard","Recycling");
+//        ItemClass item5 = new ItemClass("Food","Compost");
+//        ItemClass item6 = new ItemClass("Garbage","Garbage");
+//        ItemClass item7 = new ItemClass("Batteries","Hazard");
+//        ItemClass item8= new ItemClass("Paper","Recycling");
+//        ItemClass item9 = new ItemClass("Food","Compost");
+//        ItemClass item0 = new ItemClass("Garbage","Garbage");
+//        ItemClass item11 = new ItemClass("Batteries","Hazard");
+//        ItemClass item12 = new ItemClass("Paper","Recycling");
+//        ItemClass item13 = new ItemClass("Food","Compost");
+//        ItemClass item14 = new ItemClass("Garbage","Garbage");
+//        ItemClass item15 = new ItemClass("Batteries","Hazard");
+//        ItemClass item16 = new ItemClass("Paper","Recycling");
+//        ItemClass item17 = new ItemClass("Food","Compost");
+//        ItemClass item18 = new ItemClass("Garbage","Garbage");
+//        ItemClass item19 = new ItemClass("Batteries","Hazard");
+//
+//        ArrayList<ItemClass> overallList = new ArrayList<>();
+//
+//        overallList.add(item1);overallList.add(item11);
+//        overallList.add(item2);overallList.add(item12);
+//        overallList.add(item3);overallList.add(item14);
+//        overallList.add(item4);overallList.add(item15);
+//        overallList.add(item5);overallList.add(item16);
+//        overallList.add(item6);overallList.add(item17);
+//        overallList.add(item7);overallList.add(item18);
+//        overallList.add(item8);overallList.add(item19);
+//        overallList.add(item9);overallList.add(item13);
+//        overallList.add(item0);overallList.add(item);
 
 
         //Should have one master list from the database and constantly filtering new lists from there?
         //OR for every list pull the items from the database?
                 //Ex: Displayedlist, filtered by search, filtered by bin...
-        ArrayList<ItemClass> displayedList = overallList;
+//        ArrayList<ItemClass> displayedList = overallList;
 
 
         //Filtering down the list of items
         //USE ADAPTER.FILTER
         //Find the search bar component
-        EditText searchBar = v.findViewById(R.id.home_searchcomponent).findViewById(R.id.component_searchbar);
-        Button searchBtn = v.findViewById(R.id.home_searchcomponent).findViewById(R.id.component_searchBtn);
-
-
-
-
-
-//       ItemListAdapter adapter = new ItemListAdapter(mCtx , R.layout.adapter_item_layout, displayedList);
-//       ListView mListView = v.findViewById(R.id.home_items_list);
+//        MaterialSearchView materialSearchView = v.findViewById(R.id.home_searchcomponent);
+//        materialSearchView.closeSearch();
+//        materialSearchView.setSuggestions(list);
+//        materialSearchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
 //
-//        mListView.setAdapter(adapter);
-
-        //Figure out how to filter list down through the search bar
-        //Preferably as the user types it will automatically filter the list however if not then use the search button.
-
-//        adapter.notifyDataSetChanged();
+//                return false;
+//            }
 //
-//        searchBtn.setOnClickListener(
-//                (View.OnClickListener) (displayedList = FilterList(overallList, searchBar.getText().toString()))
-//        );
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                return false;
+//            }
+//        });
+
+
 
         return v;
     }
--- app/src/main/java/com/example/recyclingapp/MainActivity.java
+++ app/src/main/java/com/example/recyclingapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.recyclingapp;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -10,18 +11,21 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private DrawerLayout drawer;
-
+    DrawerLayout drawer;
+    String [] list;
+    MaterialSearchView materialSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
@@ -40,7 +44,40 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
             navigationView.setCheckedItem(R.id.nav_home);
         }
+
+        //SEARCH BAR CODE BELOW
+        list = new String []{"one","two","three","four","five","six","seven","eight","nine","ten"};
+        materialSearchView = findViewById(R.id.main_searchcomponent);
+        materialSearchView.clearFocus();
+        materialSearchView.closeSearch();
+        materialSearchView.setSuggestions(list);
+        materialSearchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //Here Create your filtering
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //You can make change realtime if you typing here
+                //See my tutorials for filtering with ListView
+                return false;
+            }
+        });
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        MenuItem item = menu.findItem(R.id.search_menu_search);
+        materialSearchView.setMenuItem(item);
+
+        return true;
+    }
+
     //Callback for when item in Navigation view are pressed
     @Override
     public boolean onNavigationItemSelected(MenuItem menuItem) {
@@ -73,9 +110,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return true;
     }
 
+
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+        if (materialSearchView.isSearchOpen()){
+            materialSearchView.closeSearch();
         } else {
             super.onBackPressed();
         }
